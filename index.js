378228df600efc51ed8fa5bd8f26d1e796eb10be
Leonid Vegera <leonid.vegera@gmail.com>
2024-01-24 21:04:04 +0200

    Add express for Node server

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const {getQuestion} = require('./utils/index');
 const { Bot, GrammyError, HttpError, Keyboard, InlineKeyboard } = require('grammy');
 const { getCorrectAnswer } = require('./utils');
+const express = require('express');
 require('dotenv').config();
+
+// express added for uptimerobot to update hosting
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+})
+const port = 3999;
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+})
+
 
 // const webhookURL = 'https://telegram-bot-interview-questions.vercel.app'; // this is for Vercel
 const bot = new Bot(process.env.BOT_TOKEN_KEY);
